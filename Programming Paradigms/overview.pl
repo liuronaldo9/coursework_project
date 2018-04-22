9ed3477a0d7f186ee1458b2ee65f9710f47b9a3a
kangqiwang <kangqiwang@outlook.com>
2018-04-22 02:08:06 +0100

    finish overview

--- Programming Paradigms/overview.pl
+++ Programming Paradigms/overview.pl
@@ -15,39 +15,41 @@ if (! -d $ARGV[0])
 
 # two different way to input the "NAMEPATTERN", such as "NAMEPATTERN*" or "*NAMEPATTERN"
 
-my $filename='tmp';
+my $filename='/tmp/temp_data';
+my @tmp_list;
+my $list_size;
 open(my $fh, '>', $filename) or die "Could not open file $filename $!";
-my $file = `find ./$ARGV[0] -name "$ARGV[1]" -type f -exec du -ch {} +`;
+my $file = `find $ARGV[0] -name "$ARGV[1]" -type f -exec du -c {} + | sort -n `;
 
 # hold the file name, location, size as a list
-
-my @tmp_list=split(/[\t+,\n]/, $file);
-my $list_size=@tmp_list;
+@tmp_list=split(/[\t+,\n]/, $file);
+$list_size=@tmp_list;
 
 
 print $fh "$file";
 close $fh;
 
+if ($list_size == 0){
+	die "we cannot find your file, Please check your filename and change your requirment"
+}
+
 # seperate a list as a location list and a size list and a file name list
 my @location_list;
 my @size_list;
 my @filename;
-for(my $x=2; $x<$list_size-2; $x++){
+for(my $x=0; $x<$list_size-2; $x++){
 	if (0==$x%2){
 		push @size_list,"$tmp_list[$x]";
 	}else{
 # input: location list ---> output: a file name list and a location list
-		#print("$tmp_list[$x]\n");
-		$tmp_list[$x]=~ s/.//;
 		push @location_list, "$tmp_list[$x]";
-		#print("@location_list\n");
-		$tmp_list[$x]=~ m|/([^/.]*\.[^/.]*)$|;
 		#print("$tmp_list[$x]\n");
-		push @filename,"$1";
+		my @temp_file=split(/\//, $tmp_list[$x]);
+		my $tempfilename=@temp_file;
+		$tmp_list[$x]=$temp_file[$tempfilename-1];
+		push @filename,"$tmp_list[$x]";
+	}
 }
-}
-
-
 
 
 
@@ -56,13 +58,10 @@ my @ownership;
 
 for (my $i=0; $i<scalar(@filename); $i++){
 	my $awk=q(awk '{print $3}');
-	my $tmp_owner=`ls -l $ARGV[0]/$filename[$i] | $awk`;
-#	print("$filename[$i]\n");
-	#print("$tmp_owner\n");
-#print("$tmp_owner\n");
-	#print(" $owner[2]\n");
+	my $tmp_owner=`ls -l $location_list[$i] | $awk`;
 	push @ownership,"$tmp_owner";
 }
+
 
 
 #create the image "gnuplot.jpg"
@@ -70,14 +69,15 @@ for (my $i=0; $i<scalar(@filename); $i++){
 `cat << __EOF | gnuplot
 set style data histogram
 set style fill solid 0.5 border -1
-set term png
+set xtics border in scale 0,0 nomirror rotate by -90  autojustify
+set term png size 1800, 1000
+set offset 0.2,0
 set output "gnuplot.png"
-set title "./$ARGV[0]"
-plot 'tmp' with impulses lw 9
+set title "$ARGV[0]"
+plot '/tmp/temp_data' using 1:xtic(2) with impulses lw 9
 __EOF`;
 
 #create the overview.html
-
 
 open (my $html, ">overview.html") or die "can't open out";
 #create the html path
@@ -85,7 +85,7 @@ my $html_path=`pwd`;
 my $table;
 for (my $i=0; $i<scalar(@filename); $i++){
 	my $table_tmp="<tr>
-	<td align=\"center\"><a href=\"$html_path$location_list[$i]\" target=\"_blank\">$filename[$i]</a></td>
+	<td align=\"center\"><a href=\"$location_list[$i]\" target=\"_blank\">$filename[$i]</a></td>
 	<td align=\"center\">$location_list[$i]</td>
 	<td align=\"center\">$size_list[$i]</td>
 	<td align=\"center\">$ownership[$i]</td>
@@ -101,7 +101,7 @@ my $template="<html>
 </head>
 <body>
 <h2>List of Files</h2>
-<table style=\"width:100%\">
+<table border=\"9\" style=\"width:100%\">
 <tr>
 <th>Files Name</th>
 <th>Location</th>
@@ -116,6 +116,4 @@ $table
 ";
 print $html "$template";
 close  $html;
-
-
-
+=cut
